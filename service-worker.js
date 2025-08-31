1789e6b3feacc0fc8440de2ffd9d39cdafe554b8
mwjcampbell-cmd <mwjcampbell@gmail.com>
2025-08-31 12:34:19 +1200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = "building-project-cache-v1";
+const assetsToCache = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(cache => cache.addAll(assetsToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+});
